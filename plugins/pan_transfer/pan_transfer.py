6f664661d487a2b6cebdaae1c24724711d075b6a
imouup <iznyaku@outlook.com>
2025-01-07 19:42:57 +0800

    修复了奇怪的bug，现在能获取所有使用nya盘的视频了

--- plugins/pan_transfer/pan_transfer.py
+++ plugins/pan_transfer/pan_transfer.py
@@ -2,6 +2,7 @@ from src.createTab import CreateTab
 from src.login import login
 from src.cookies import getCookies, getUserinfo
 from src.file_store import getPath
+from DrissionPage import SessionPage
 
 # 创建标签页
 ini_path = None
@@ -36,9 +37,11 @@ class panVideo():
     def __init__(self,mvid,title):
         self.mvid = mvid
         self.title = title
-    def getPan_url(self,tab):
-        tab.get(f'https://www.mfuns.net/video/{self.mvid}')
-        self.pan_url = tab.ele('@property=og:video').attr('content')
+    def getPan_url(self):
+        page = SessionPage()
+        page.get(f'https://www.mfuns.net/video/{self.mvid}')
+        self.pan_url = page.ele('@property=og:video').attr('content')
+        print(self.pan_url)
     def download(self):
         pass
     def upload(self):
@@ -46,9 +49,24 @@ class panVideo():
 
 # 定义函数查找mvid
 def find_mvid(url):
-    if url[22:27] == 'video':
-        return url[28:]
-    return False
+    try:
+        for i in range(len(url)):
+
+            if url[i:i+5] == 'video':
+
+                return url[i+6:]
+
+        return False
+    except TypeError:
+        return False
+
+# 定义函数判断是否为网盘外链
+def ispan(pan_url):
+    if pan_url[8:21] == 'pan.nyaku.moe' or pan_url[8:24] == 'nyapan.mouup.top':
+        print('find1')
+        return True
+    else:
+        return False
 
 
 # 加载视频下载页
@@ -57,6 +75,8 @@ tab.get(f'https://www.mfuns.net/member/{uid}/videoList')
 while end():
     tab.actions.scroll(delta_y=23333)
 
+# 切换模式
+#tab.change_mode()
 # 获取视频列表
 video_list = tab.eles('.m-link notlink')
 mfv_list= []
@@ -66,16 +86,20 @@ for i in video_list:
     if bool(find_mvid(mvurl)) == True:
         mvid = find_mvid(mvurl)
         mfvideo = panVideo(mvid,title)
+        mfvideo.getPan_url()
         mfv_list.append(mfvideo)
+print('总共检索到{}个视频'.format(len(mfv_list)))
 
 panv_list = []
 for mfvideo in mfv_list:
-    if mfvideo.pan_url[]
+    if ispan(mfvideo.pan_url) == True:
+        panv_list.append(mfvideo)
+        print(f'{mfvideo.title}是网盘外链视频')
 
-print('总共搜索到{}个使用Nya盘的视频'.format(len(mfv_list)))
 
-# 切换模式
-tab.change_mode()
+print('其中有{}个使用Nya盘的视频'.format(len(panv_list)))
+
+
 
 
 
