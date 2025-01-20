6c21fa301b2d9b4cc846d4c06a4b0ce1394785b5
一之濑亚子 <iznyaku@outlook.jp>
2025-01-20 21:25:27 +0800

    撤回对getMultiP函数的修改

--- src/createTab.py
+++ src/createTab.py
@@ -12,11 +12,16 @@ class CreateTab():
         # 创建配置对象,设置隐藏浏览器界面
         self.co = ChromiumOptions(ini_path=ini_path).set_paths(local_port=9100,user_data_path='{}'.format(getPath(['data','userdata'],2)))
 
-    def headless(self):
-        self.co.headless()
+    def headless(self,TorF):
+        self.co.headless(TorF)
 
     def create(self):
         # 启动或接管浏览器，并创建标签页对象
         tab = Chromium(addr_or_opts=self.co).latest_tab
+        return tab
+
+    def newtab(self):
+        newBrowser = Chromium(addr_or_opts=self.co)
+        tab = newBrowser.new_tab()
         return tab
 
