32b241e3f255c7424d922837d717d0afb3bb2c17
一之濑亚子 <iznyaku@outlook.jp>
2025-01-21 22:06:20 +0800

    添加dir_num

--- src/createTab.py
+++ src/createTab.py
@@ -5,12 +5,12 @@ from src.file_store import getPath
 
 
 class CreateTab():
-    def __init__(self,ini_path):
+    def __init__(self,ini_path,dir_num=0):
         # 设置语言
         Settings.set_language('zh_cn')
 
         # 创建配置对象,设置隐藏浏览器界面
-        self.co = ChromiumOptions(ini_path=ini_path).set_paths(local_port=9100,user_data_path='{}'.format(getPath(['data','userdata'],2)))
+        self.co = ChromiumOptions(ini_path=ini_path).set_paths(local_port=9100,user_data_path='{}'.format(getPath(['data','userdata'],dir_num)))
 
     def headless(self,TorF):
         self.co.headless(TorF)
